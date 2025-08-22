ba5b233eef6f4183f3c5b0d0c3d1e0dc3ec4b069
LeonardoCarrillo-alt <leo.c.c.zubieta@gmail.com>
2025-08-22 19:51:30 -0400

    cambios posiblemente relevantes

--- Game/main.py
+++ Game/main.py
@@ -1,24 +1,42 @@
-import arcade
-import firstScreen
-import screenStory
+import arcade 
 
 WIDTH = 1280
 HEIGHT = 720
 TITLE = "Space War - Game"
 
-class GameWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(WIDTH, HEIGHT, TITLE)
-        
-        # Crear las vistas
-        self.screen_view = firstScreen.ScreenView()
-        self.story_view = screenStory.ScreenStory()
-        
-        # Mostrar la primera pantalla
-        self.show_view(self.screen_view)
+class FirstScreen(arcade.View): 
+    def on_show_view(self):
+        self.firstscreen = arcade.load_texture("assets/imgScreen/screen1.png")
+        self.sound1 = arcade.load_sound("audio/game-8-bit-on-278083.wav")
+        arcade.play_sound(self.sound1, volume=0.2)
+    
+    def on_draw(self):
+        self.clear()
+        # FORMA CORRECTA 1: Usar draw_texture_rectangle
+        arcade.draw_texture_rect(
+           self.firstscreen, arcade.LRBT(0, WIDTH, 0, HEIGHT)
+        )
+    
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.SPACE:
+            self.sound1.stop()
+            story_view = StoryScreen()
+            self.window.show_view(story_view)
+
+class StoryScreen(arcade.View):
+    def on_show_view(self):
+        self.storyscreen = arcade.load_texture("assets/imgScreen/screen2.png")
+    
+    def on_draw(self):
+        self.clear()
+        arcade.draw_texture_rect(
+            self.storyscreen, arcade.LRBT(0, WIDTH, 0, HEIGHT)
+        )
 
 def main():
-    window = GameWindow()
+    window = arcade.Window(WIDTH, HEIGHT, TITLE)
+    game = FirstScreen()
+    window.show_view(game)
     arcade.run()
 
 if __name__ == "__main__":
